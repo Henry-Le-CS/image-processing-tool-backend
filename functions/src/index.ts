871c45916313377bf7ba7215a85ac54e244ac2a7
Henry Le <lehieu2077@gmail.com>
2023-09-19 04:13:43 +0700

    initial commit

---
+++ functions/src/index.ts
@@ -0,0 +1,33 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+// import { onRequest } from "firebase-functions/v2/https";
+// import * as functions from "firebase-functions"
+// import * as logger from "firebase-functions/logger";
+import  * as express from "express"
+import * as functions from "firebase-functions"
+import * as cors from "cors"
+import * as bodyParser from "body-parser"
+import * as dotenv from "dotenv"
+import { fileRouter } from "./routes";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req: any, res: any) => {
+    res.send('Hello World!');
+});
+
+app.use('/api', fileRouter)
+
+exports.app = functions.https.onRequest(app);
