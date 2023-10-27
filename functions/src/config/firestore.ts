d18414fa31301114f9e3e68f4a4b5871b52f0c39
Henry Le <lehieu2077@gmail.com>
2023-10-27 09:19:03 +0700

    update: change serviceAccount for firestore

--- functions/src/config/firestore.ts
+++ functions/src/config/firestore.ts
@@ -1,7 +1,7 @@
 import * as admin from "firebase-admin";
 
 // TODO: change the .env to GOOGLE_API_CREDENTIALS
-const serviceAccount = JSON.parse(process.env.GOOGLE_DRIVE_API_CREDENTIALS ?? "");
+const serviceAccount = JSON.parse(process.env.FIRE_STORE_SERVICE_ACCOUNT ?? "");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
